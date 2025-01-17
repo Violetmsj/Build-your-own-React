22cef7cc0d467716c24371736f024c27b79bf1ab
VioletHonor <1498218391@qq.com>
2025-01-17 17:38:54 +0800

    feat():初步实现Reconciliation（Diff算法）

--- react/myRender.js
+++ react/myRender.js
@@ -17,6 +17,9 @@ let nextUnitOfWork = null;
 // 记录整颗Fiber树
 let wipRoot = null;
 
+// diffing中记录要删除的节点
+let deletions = null;
+
 function myRender(element, container) {
   // 初始化下一个工作单元（根节点）
   wipRoot = {
@@ -27,8 +30,56 @@ function myRender(element, container) {
     sibling: null,
     child: null,
     parent: null,
+    // alternate对旧 Fiber 的链接，这个旧 Fiber 是我们在在上一个 commit phase 向 DOM commit 的 Fiber。
+    alternate: currentRoot,
   };
+  deletions = [];
   nextUnitOfWork = wipRoot;
+}
+function reconcileChildren(wipFiber, elements) {
+  //index单纯为了while循环
+  let index = 0;
+  let prevSibling = null;
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    const sameType = oldFiber && element && element.type == oldFiber.type;
+    let newFiber = null;
+    if (sameType) {
+      // 更新
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      };
+    }
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT",
+      };
+    }
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber); // 这里使用了一个数组来追踪我们想要删除的 node
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
 }
 function workLoop(deadline) {
   let shouldYield = false;
@@ -49,43 +100,45 @@ function performUnitOfWork(fiber) {
   if (!fiber.dom) {
     fiber.dom = creatDom(fiber);
   }
-  // 如果当前节点存在父节点，将其挂载到父节点下
-  // if (fiber.parent) {
-  //   fiber.parent.dom.append(fiber.dom);
-  // }
 
   // 为每一个chid创建一个新的 fiber
   const elements = fiber.props.children;
-  let index = 0;
+  // let index = 0;
   // 记录上一个fiber
-  let prevSibling = null;
-  while (index < elements.length) {
-    const element = elements[index];
+  // let prevSibling = null;
+  //下面的while是构建Fiber树，为了实现diff算法，需要重构。
+  // while (index < elements.length) {
+  //   const element = elements[index];
 
-    const newFiber = {
-      // 记录元素标签类型
-      type: element.type,
-      // 记录元素属性
-      props: element.props,
-      // 与父节点创建链接
-      parent: fiber,
-      // 此时DOM还未生成 初始化null
-      // 下一次进入performUnitOfWork时
-      // 通过createDOM函数生成
-      dom: null,
-      // 与第一个子节点关联 初始化null
-      child: null,
-      // 与兄弟节点创建链接 初始化null
-      sibling: null,
-    };
-    if (index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling.sibling = newFiber;
-    }
-    prevSibling = newFiber;
-    index++;
-  }
+  //   const newFiber = {
+  //     // 记录元素标签类型
+  //     type: element.type,
+  //     // 记录元素属性
+  //     props: element.props,
+  //     // 与父节点创建链接
+  //     parent: fiber,
+  //     // 此时DOM还未生成 初始化null
+  //     // 下一次进入performUnitOfWork时
+  //     // 通过createDOM函数生成
+  //     dom: null,
+  //     // 与第一个子节点关联 初始化null
+  //     child: null,
+  //     // 与兄弟节点创建链接 初始化null
+  //     sibling: null,
+  //   };
+  //   if (index === 0) {
+  //     fiber.child = newFiber;
+  //   } else {
+  //     prevSibling.sibling = newFiber;
+  //   }
+  //   prevSibling = newFiber;
+  //   index++;
+  // }
+
+  //重构如下
+  // 此函数接收两个参数：即将被处理的fiber(wipFiber)以及它的子元素(elements)
+  reconcileChildren(fiber, elements);
+
   // 选出下一个工作单元
   if (fiber.child) {
     // 如果有child直接返回child
@@ -102,10 +155,12 @@ function performUnitOfWork(fiber) {
     nextFiber = nextFiber.parent;
   }
 }
-
+// 最后一次 commit 到 DOM 的一棵 Fiber Tree 的引用进行保存
+let currentRoot = null;
 // commit阶段
 function commitRoot() {
   commitWork(wipRoot.child);
+  currentRoot = wipRoot;
   wipRoot = null;
 }
 function commitWork(fiber) {
